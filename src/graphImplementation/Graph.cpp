2d9c0d52792f93941ae1ebba9e225873bab69bcf
AkiraKudo <akirakudo901@gmail.com>
2023-12-29 17:21:31 -0800

    Replaced pointer parameters within Graph.cpp to references.
    Removed test cases dealing with nullptr edge cases for
    Graph.cpp.
    
    Added new testing if appropriate edges are
    removed when running remove_vertex.

--- src/graphImplementation/Graph.cpp
+++ src/graphImplementation/Graph.cpp
@@ -19,82 +19,76 @@ GraphImplementation::Graph::~Graph()
 };
 
 // returns whether vertex x is in the graph
-bool GraphImplementation::Graph::contains_vertex(Vertex *x)
+bool GraphImplementation::Graph::contains_vertex(Vertex& x)
 {
-    // guard against nullptr
-    if (x == nullptr) return false;
-    return (adjList.find(x) != adjList.end());
+    return (adjList.find(&x) != adjList.end());
 };
 
 // return if x and y are adjacent
-bool GraphImplementation::Graph::adjacent(Vertex *x, Vertex *y)
+bool GraphImplementation::Graph::adjacent(Vertex& x, Vertex& y)
 {
-    // guard against nullptr
-    if (x == nullptr || y == nullptr) return false;
     // if x or y aren't contained in graph, return false
     if (!contains_vertex(x) || !contains_vertex(y)) return false;
-    // if x and y are the same, return false - we don't allow loops here
-    if (x == y) return false;
+    // if x and y point to the same memory address, return false - we don't allow loops in the graph
+    if (&x == &y) return false;
     // otherwise
-    std::vector<Vertex*> xAdjList = adjList[x];
+    std::vector<Vertex*> xAdjList = adjList[&x];
     for (auto element : xAdjList)
     {
-        if (element == y) return true;
+        if (element == &y) return true;
     }
     return false;
 };
 
 // return all neighbors of x
-std::vector<GraphImplementation::Vertex*> GraphImplementation::Graph::neighbors(Vertex *x)
+std::vector<GraphImplementation::Vertex*> GraphImplementation::Graph::neighbors(Vertex& x)
 {
-    // if x is a nullptr or isn't in the graph yet, just return an empty vector
-    if (x == nullptr || !contains_vertex(x))
+    // if x isn't in the graph yet, just return an empty vector
+    if (!contains_vertex(x))
         return std::vector<Vertex*>();
     else 
-        return adjList[x];
+        return adjList[&x];
 };
 
 // adds vertex x to G
-void GraphImplementation::Graph::add_vertex(Vertex *x)
+void GraphImplementation::Graph::add_vertex(Vertex& x)
 {
-    // if x is a nullptr or is contained in graph, skip
-    if (x == nullptr || contains_vertex(x)) return;
-    adjList[x] = std::vector<Vertex*>();
+    // if x is contained in graph, skip
+    if (contains_vertex(x)) return;
+    adjList[&x] = std::vector<Vertex*>();
 };
 
-// removes vertex x if there
-void GraphImplementation::Graph::remove_vertex(Vertex *x)
+// removes vertex x if there, as well as edges connected to x
+void GraphImplementation::Graph::remove_vertex(Vertex& x)
 {
-    // if x is a nullptr or isn't contained in this graph, skip
-    if (x == nullptr || !contains_vertex(x)) return;
+    // if x isn't contained in this graph, skip
+    if (!contains_vertex(x)) return;
     // otherwise get the adjacency list of x
-    std::vector<Vertex*> xAdjList = adjList[x];
+    std::vector<Vertex*> xAdjList = adjList[&x];
     // remove corresponding edges from the other vertex' adjacency list
-    for (Vertex* adjVrtx : xAdjList) remove_edge(x, adjVrtx);
+    for (Vertex* adjVrtx : xAdjList) remove_edge(x, *adjVrtx);
     // remove entry of x from adjList
-    adjList.erase(x);
+    adjList.erase(&x);
 };
 
 // adds edge z from vertex x to y if not there
-void GraphImplementation::Graph::add_edge(Vertex *x, Vertex *y, Edge z)
+void GraphImplementation::Graph::add_edge(Vertex &x, Vertex &y, Edge z)
 {
-    // if either of x and y are nullptr, skip
-    if (x == nullptr || y == nullptr) return;
     // if either of x and y aren't in the graph, skip
     if (!contains_vertex(x) || !contains_vertex(y)) return;
     // if x and y are the same vertex, skip
-    if (x == y) return;
+    if (&x == &y) return;
     // if the edge is already there, skip
     if (adjacent(x, y)) return;
     // otherwise, add the edge. Currenctly, we won't use the argument z
-    adjList[x].push_back(y);
-    adjList[y].push_back(x);
+    adjList[&x].push_back(&y);
+    adjList[&y].push_back(&x);
 };
 
 // removes edge from x to y if there
-void GraphImplementation::Graph::remove_edge(Vertex *x, Vertex *y)
+void GraphImplementation::Graph::remove_edge(Vertex& x, Vertex& y)
 {
-     auto findAndRemoveEntryFromAdjacencyList = [](std::vector<Vertex*> *lst, Vertex *toRemove)
+    auto findAndRemoveEntryFromAdjacencyList = [](std::vector<Vertex*> *lst, Vertex *toRemove)
     {
         // find the position of the vertex we are trying to remove in the adjacency list
         auto toErase = std::find(lst->begin(), lst->end(), toRemove);
@@ -102,14 +96,12 @@ void GraphImplementation::Graph::remove_edge(Vertex *x, Vertex *y)
         if (toErase != lst->end()) lst->erase(toErase);
     };
 
-    // if either of x and y is a nullptr, skip
-    if (x == nullptr || y == nullptr) return;
     // if either of x and y aren't in the graph, skip
     if (!contains_vertex(x) || !contains_vertex(y)) return;
     // if x and y are the same vertex, skip
-    if (x == y) return;
+    if (&x == &y) return;
     // otherwise get the adjacency list of x and remove the corresponding edge
-    findAndRemoveEntryFromAdjacencyList(&adjList[x], y);
+    findAndRemoveEntryFromAdjacencyList(&adjList[&x], &y);
     // DO THE EXACT SAME FOR y
-    findAndRemoveEntryFromAdjacencyList(&adjList[y], x);
+    findAndRemoveEntryFromAdjacencyList(&adjList[&y], &x);
 };
--- src/graphImplementation/Graph.h
+++ src/graphImplementation/Graph.h
@@ -27,10 +27,9 @@ namespace GraphImplementation
         //-> best in adding "ToCheck" arcs for arc consistency
         
         // we will implement this using unordered_map (hash map) of vertex pointers
-        // to their corresponding vector (I don't know what's the best best implementation)
-        // USING POINTERS, AS THIS IS THE ONLY SHARED PROPERTY FOR Vertex-TYPE OBJECTS
-        // AND UNIQUELY DETERMINES VERTICES AS WE CREATE THEM. I THINK WE CAN HAVE MULTIPLE 
-        // VERTICES WITH THE EXACT SAME ATTRIBUTES BUT DIFFERENT ADDRESSES.
+        // to their corresponding vector
+        // I want quick access to a "Vertex" object's registered neighbor nodes
+        // as uniquely determined by its memory address 
         std::unordered_map<Vertex*, std::vector<Vertex*>> adjList;
         
     public:
@@ -38,19 +37,19 @@ namespace GraphImplementation
         ~Graph();
 
         // returns whether vertex x is in the graph
-        bool contains_vertex(Vertex *x);
+        bool contains_vertex(Vertex& x);
         // return if vertices x and y are adjacent
-        bool adjacent(Vertex *x, Vertex *y);
+        bool adjacent(Vertex& x, Vertex& y);
         // return all neighbors of vertex x
-        std::vector<Vertex*> neighbors(Vertex *x);
+        std::vector<Vertex*> neighbors(Vertex& x);
         // adds vertex x to the graph
-        void add_vertex(Vertex *x);
-        // removes vertex x if there
-        void remove_vertex(Vertex *x);
+        void add_vertex(Vertex& x);
+        // removes vertex x if there, as well as edges connected to x
+        void remove_vertex(Vertex& x);
         // adds edge z from vertex x to y if not there
-        void add_edge(Vertex *x, Vertex *y, Edge z);
+        void add_edge(Vertex &x, Vertex &y, Edge z);
         // removes edge from x to y if there
-        void remove_edge(Vertex *x, Vertex *y);        
+        void remove_edge(Vertex &x, Vertex &y);        
     };
 }
 
