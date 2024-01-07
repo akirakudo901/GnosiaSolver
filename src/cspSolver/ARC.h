9ad5e995eeebe3f1db805816a6a06c8f87894ae3
AkiraKudo <akirakudo901@gmail.com>
2024-01-06 21:16:59 -0800

    Added generate_unique_name to ARC

--- src/cspSolver/ARC.h
+++ src/cspSolver/ARC.h
@@ -6,6 +6,7 @@
 #ifndef ARC_H
 #define ARC_H
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -27,6 +28,22 @@ namespace CSPSolverImplementation
             this->main_var = nullptr;
             this->other_var_list = std::vector<GraphImplementation::VariableVertex*>();
             this->constraint = nullptr;
+        };
+
+        std::string generate_unique_string()
+        {
+            std::string returned = main_var->getName() + "-";
+            std::set<std::string> names;
+            for (GraphImplementation::VariableVertex* vv : other_var_list)
+            {
+                names.insert(vv->getName());
+            }
+            for (std::string name : names)
+            {
+                returned += name + "-";
+            }
+            
+            return returned += constraint->getName();
         };
 
         // overloading == so that we can use those in Boost tests
@@ -73,7 +90,7 @@ namespace CSPSolverImplementation
             os << "Main var: " << std::endl; 
             os << *(arc.main_var) << "\n";
             os << "Other vars: " << std::endl;
-            for (auto& elem : arc.other_var_list) os << *(elem) << " ";
+            for (auto& elem : arc.other_var_list) os << *(elem) << "\n";
             os << "\n";
             os << "Constraint: " << std::endl;
             os << *(arc.constraint) << "\n";
